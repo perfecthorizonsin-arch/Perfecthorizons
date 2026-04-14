d547813453bf58945563f046cec64d1e0c178dfd
perfecthorizonsin-arch <perfecthorizons.in@gmail.com>
2026-04-14 18:30:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'horizon-crm-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
